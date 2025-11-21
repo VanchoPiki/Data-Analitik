89534607cccdd2cdd8e89be4bb03c9cde4ce7a28
VanchoPiki <varava24102020@gmail.com>
2025-11-22 01:55:44 +0300

    Lesson - 2

---
+++ Lesson_2/Lesson_2_1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+list = [1, 2, 3, 4, 5]
+arr = np.array(list)
+print(arr * 2)
+
+# [ 2  4  6  8 10]
+
+matrix = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8,9],
+])
+print(matrix * 2)
+
+# [[ 2  4  6]
+#  [ 8 10 12]
+#  [14 16 18]]
