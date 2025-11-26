26f3b29132551803bd4edb9eb4e695ddb604909b
VanchoPiki <varava24102020@gmail.com>
2025-11-26 23:49:39 +0300

    Lesson - 2

---
+++ Lesson_2/Lesson_2_6.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(np.mean(arr))
+
+#2.5
+
+print(np.std(arr))
+
+#1.118033988749895
+
+print(np.median(arr))
+
+#2.5
+
+print(np.min(arr), np.max(arr))
+
+#1 4
