052fc94dcff41304a6c93f62acf1738be10d3ffe
VanchoPiki <varava24102020@gmail.com>
2025-12-02 23:30:33 +0300

    Lesson - 2

--- Lesson_2/Finish_Lesson_2/Test_of_AI/TEST_OF_AI_2.py
+++ Lesson_2/Finish_Lesson_2/Test_of_AI/TEST_OF_AI_2.py
@@ -11,3 +11,26 @@ import numpy as np
 # 4. Замените NaN на среднее значение остальных температур
 
 temp = [22.5, 24.0, 19.5, 25.5, 23.0, 20.5, 21.0]
+
+mean_temp = np.mean(temp)
+median_temp = np.median(temp)
+std_temp = np.std(temp)
+min_temp = np.min(temp)
+max_temp = np.max(temp)
+
+print(f"Средняя температура за неделю: {mean_temp}\n\n Медиану температур: {median_temp}\n\n Стандартное отклонение: {std_temp}\n\n Минимальная: {min_temp}\n\n Максимальное: {max_temp}\n")
+
+temp_2 = [22.5, 24.0, None, 25.5, 23.0, None, 21.0]
+temp_2_2 = []
+def main(temp_2):
+    temp_2_2 = []
+    for i in temp_2:
+        if i is not None:
+            temp_2_2.append(i)
+    for i in temp_2:
+        if i is None:
+            i = np.mean(temp_2_2)
+            temp_2_2.append(float(i))
+        return temp_2_2
+
+print(main(temp_2))
